a89d6fcaead342bd8f0fb604e51da8431a1fc585
Waz925 <venu.bharadwaz@gmail.com>
2025-06-20 17:29:29 +0200

    Add files via upload

--- custom_components/geod/__init__.py
+++ custom_components/geod/__init__.py
@@ -1,7 +1,6 @@
 """GEOD Token Integration."""
-from .const import DOMAIN
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.core import HomeAssistant
 
-async def async_setup_entry(hass, config_entry):
-    from .sensor import async_setup_entry as sensor_setup
-    await sensor_setup(hass, config_entry, lambda: None)
+async def async_setup_entry(hass: HomeAssistant, config_entry: ConfigEntry):
     return True
--- custom_components/geod/sensor.py
+++ custom_components/geod/sensor.py
@@ -2,53 +2,55 @@ import aiohttp
 from homeassistant.helpers.entity import Entity
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
+from homeassistant.helpers.entity_platform import AddEntitiesCallback
 
-GEOD_TOKEN_DECIMALS = 1e18
-
-async def async_setup_entry(hass: HomeAssistant, config_entry: ConfigEntry, async_add_entities):
-    wallet = config_entry.data.get("wallet_address")
-    api_key = config_entry.data.get("polygonscan_api_key")
-    sensor = GEODBalanceSensor(wallet, api_key)
-    async_add_entities([sensor], True)
-
-    async def handle_manual_refresh(call):
-        await sensor.async_update()
-        sensor.async_write_ha_state()
-
-    hass.services.async_register("geod", "refresh_geod_data", handle_manual_refresh)
 
 class GEODBalanceSensor(Entity):
-    def __init__(self, wallet, api_key):
-        self._wallet = wallet
+    def __init__(self, wallet_address, api_key):
+        self._wallet_address = wallet_address
         self._api_key = api_key
         self._state = None
         self._attr_name = "GEOD Token Balance"
+        self._attr_unique_id = f"geod_balance_{wallet_address[-6:]}"
         self._attr_unit_of_measurement = "GEOD"
-        self._attr_icon = "mdi:currency-usd"
 
     @property
     def name(self):
-        return "GEOD Token Balance"
+        return self._attr_name
+
+    @property
+    def unique_id(self):
+        return self._attr_unique_id
 
     @property
     def state(self):
         return self._state
 
+    @property
+    def unit_of_measurement(self):
+        return self._attr_unit_of_measurement
+
     async def async_update(self):
         url = (
             "https://api.polygonscan.com/api"
-            "?module=account"
-            "&action=tokenbalance"
-            "&contractaddress=0xAC0F66379A6d7801D7726d5a943356A172549Adb"
-            f"&address={self._wallet}"
-            "&tag=latest"
-            f"&apikey={self._api_key}"
+            f"?module=account&action=tokenbalance"
+            f"&contractaddress=0xAC0F66379A6d7801D7726d5a943356A172549Adb"
+            f"&address={self._wallet_address}&tag=latest&apikey={self._api_key}"
         )
-        try:
-            async with aiohttp.ClientSession() as session:
-                async with session.get(url) as resp:
-                    data = await resp.json()
-                    result = int(data.get("result", "0")) / GEOD_TOKEN_DECIMALS
-                    self._state = round(result, 2)
-        except Exception:
-            self._state = None
+        async with aiohttp.ClientSession() as session:
+            async with session.get(url) as response:
+                data = await response.json()
+                if data.get("status") == "1":
+                    self._state = round(int(data["result"]) / 1e18, 4)
+                else:
+                    self._state = None
+
+
+async def async_setup_entry(
+    hass: HomeAssistant,
+    config_entry: ConfigEntry,
+    async_add_entities: AddEntitiesCallback
+):
+    wallet = config_entry.data.get("wallet_address")
+    api_key = config_entry.data.get("polygonscan_api_key")
+    async_add_entities([GEODBalanceSensor(wallet, api_key)], True)
